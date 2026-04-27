f7b0c02f81779aa7042a52cfb404461c3d00f8a5
Rakibislam22 <rakibislam13122018@gmail.com>
2026-04-27 21:24:35 +0600

    Initial the app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className='text-yellow-400 text-5xl'>
+            hello world
+        </div>
+    );
+};
+
+export default App;
